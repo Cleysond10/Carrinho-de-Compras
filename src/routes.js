d668da5705f64405a8c934b73d7b427dda04b5d4
Cleyson Henrique Silva Duarte <cleysonduarte99@gmail.com>
2021-11-07 09:24:23 -0300

    Create link beetween feira and carrinho routes and render selected products

--- src/routes.js
+++ src/routes.js
@@ -17,11 +17,11 @@ function Router() {
             <Route path='/feira'>
               <Feira />
             </Route>
+            <Route path='/carrinho'>
+              <Carrinho />
+            </Route>
           </CarrinhoProvider>
         </UsusarioProvider>
-        <Route path='/carrinho'>
-          <Carrinho />
-        </Route>
       </Switch>
     </BrowserRouter>
   )
